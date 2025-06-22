5f7a0647a8551bebb0ac72b632a59f6bbc7c1a38
Felix Egan <felix.egan+git@icloud.com>
2025-06-22 18:17:53 -0400

    fix: vercel ui/toaster import error

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,22 @@
+/// <reference types="node" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import tsconfigPaths from "vite-tsconfig-paths";
 
-// Vite configuration for deployment to GitHub Pages
+// Vite configuration for deployment
 export default defineConfig({
   base: "/",
+  resolve: {
+    alias: {
+      "@": "/src"
+    }
+  },
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
+    tsconfigPaths(),
   ],
 });
